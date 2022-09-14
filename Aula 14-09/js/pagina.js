a6bc87d1b8dc5f6ac3e6fff98c6918cf1adeb744
Cleber <104567962+clebspanik@users.noreply.github.com>
2022-09-14 11:22:03 -0300

    Javascript
    Aula inicial

---
+++ Aula 14-09/js/pagina.js
@@ -0,0 +1,21 @@
+
+
+/*document.write("<h1>Alou!</h1>");
+//escrever cabeçalho
+document.write("<p>Isto é um parágrafo.<p>");
+document.write("<p>Este é outro parágrafo.</p>");
+window.alert("Área Restrita");
+alert("Hello! I am an alert box!!");
+nome = prompt("Qual o seu nome?")
+document.write("<h3>"+ nome +" seja-bem vindo a minha página</h3>");*/
+
+/*document.alert ("Seja-bem vindo");
+window.alert ("Cuidado seus dados estão sendo capturados por forças alienígenas");*/
+
+nome = prompt("Qual onúmero a ser somado?")
+document.write (""+ nome +" seja-bem vindo a minha página</h3>");
+//var a = 25 + 78; 
+
+alert(""+a+"");
+
+
